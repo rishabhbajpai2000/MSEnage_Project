51b5b03060ee69810c30d4be01168f84b002ac37
“Rishabh <rishabhbajpai190@gmail.com>
2022-05-26 04:48:27 +0530

    minor changes

--- Flask website/app.py
+++ Flask website/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template,request
+# from Functions import cars
+# import pandas as pd
+# from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -11,8 +14,10 @@ def home():
         milege = request.form.get('milege')
         type = request.form.get('type')
         seating = request.form.get('seating')
-        print(cost)
+        print(cost, brand, milege, type, seating)       
         # issue: it loads whole page and does not goes to specific part of the page
+        cars(cost,brand, milege, type, seating)
+
 
     return render_template("index.html")
 
