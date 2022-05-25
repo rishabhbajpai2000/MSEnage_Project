0516941e97da2ce408231a74de381e07f1b05bad
“Rishabh <rishabhbajpai190@gmail.com>
2022-05-26 05:00:18 +0530

    suddenly old one stoped working, made a copy

---
+++ Flask website not working/Functions.py
@@ -0,0 +1,20 @@
+# find car function
+# reading the file
+import pandas as pd
+main_data = pd.read_csv("Data.csv")
+
+# cleaning the data
+# making the values of the as float from intger.  
+main_data["ARAI_Certified_Mileage_kmpl"].astype(float)
+
+
+#writing the main function
+def cars(cost, brand, milege, p_d_cng, seating, data=main_data):
+    filter_p = data[data["Ex-Showroom_Price"].isin(range(cost[0],cost[1]))]
+    filter_b_p = filter_p[filter_p["Make"] == brand]
+    filter_b_p_t = filter_b_p[filter_b_p["Fuel_Type"]  == p_d_cng]
+    filter_b_m_p_t = filter_b_p_t[filter_b_p_t["ARAI_Certified_Mileage_kmpl"].isin(range(milege[0], milege[1]))]
+    filter_b_m_p_s_t = filter_b_m_p_t[filter_b_m_p_t["Seating"] == seating]
+    filter_b_m_p_s_t.sort_values("Ex-Showroom_Price").head()
+    return filter_b_m_p_s_t 
+
---
+++ Flask website not working/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+from Functions import cars
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if (request.method == "POST"):
+        #  take the values from the form 
+        cost = request.form.get('cost')
+        brand = request.form.get('brand')
+        milege = request.form.get('milege')
+        type = request.form.get('type')
+        seating = request.form.get('seating')
+        print(cost, brand, milege, type, seating)       
+        # issue: it loads whole page and does not goes to specific part of the page
+        cars(cost,brand, milege, type, seating)
+
+
+    return render_template("index.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+app.run(debug=True)
