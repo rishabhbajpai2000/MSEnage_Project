0165deeacb1ed29ebd50ed784430c6f653dbc595
“Rishabh <rishabhbajpai190@gmail.com>
2022-05-27 22:36:22 +0530

    a big commit

--- Functions.py
+++ Functions.py
@@ -6,17 +6,27 @@ main_data = pd.read_csv("Data.csv")
 # cleaning the data
 # making the values of the as float from intger.  
 main_data["ARAI_Certified_Mileage_kmpl"].astype(float)
+# removing the duplicates based on the rows
+main_data = main_data.drop_duplicates(subset="Model")
 
 
+test_data = main_data.head()
 #writing the main function
-def cars(cost, brand, milege, p_d_cng, seating, data=main_data):
-    filter_p = data[data["Ex-Showroom_Price"].isin(range(cost[0],cost[1]))]
+def cars(cost_min, cost_max, brand, mileage_min, mileage_max, p_d_cng, seating, data=main_data):
+    filter_p = data[data["Ex-Showroom_Price"].isin(range(cost_min,cost_max))]
     filter_b_p = filter_p[filter_p["Make"] == brand]
     filter_b_p_t = filter_b_p[filter_b_p["Fuel_Type"]  == p_d_cng]
-    filter_b_m_p_t = filter_b_p_t[filter_b_p_t["ARAI_Certified_Mileage_kmpl"].isin(range(milege[0], milege[1]))]
-    filter_b_m_p_s_t = filter_b_m_p_t[filter_b_m_p_t["Seating"] == seating]
-    filter_b_m_p_s_t.sort_values("Ex-Showroom_Price").head()
-    return filter_b_m_p_s_t 
+    filter_b_m_p_t = filter_b_p_t[filter_b_p_t["ARAI_Certified_Mileage_kmpl"].isin(range(mileage_min, mileage_max))]
+    filter_b_m_p_s_t = filter_b_m_p_t[filter_b_m_p_t["Seating_Capacity"] == seating]
+    return filter_b_m_p_s_t.sort_values("Ex-Showroom_Price").head() 
+
+
 
 # simple comment
 # random commit from g cloud
+
+# def car_found_desc(data):
+    # this fuction will take the output 5 rows data frame to generate a text description of it
+    # input: 5 row pandas dataframw
+    # output: an array of len 5, where ith index will contain description of ith car, in around 70 words
+
--- main.py
+++ main.py
@@ -1,32 +1,78 @@
-from flask import Flask, render_template,request
-from Functions import cars
+from flask import Flask, render_template, request
+from Functions import cars, test_data
 import pandas as pd
 
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if (request.method == "POST"):
-        #  take the values from the form 
-        cost_min = request.form.get('cost_min')
-        cost_max = request.form.get('cost_max')
-        brand = request.form.get('brand')        
-        mileage_min = request.form.get('mileage_min')
-        mileage_max = request.form.get('mileage_max')
-        type = request.form.get('type')
-        seating = request.form.get('Seating_Cap')
-        print(cost_max,cost_min, brand, mileage_min, mileage_max, type, seating)       
+    if request.method == "POST":
+        #  take the values from the form
+        cost_min_i = int(request.form.get("cost_min"))
+        cost_max_i = int(request.form.get("cost_max"))
+        brand_i = request.form.get("brand")
+        mileage_min_i = int(request.form.get("mileage_min"))
+        mileage_max_i = int(request.form.get("mileage_max"))
+        type_i = request.form.get("type")
+        seating_i = int(request.form.get("Seating_Cap"))
+        print(cost_max_i,cost_min_i, brand_i, mileage_min_i, mileage_max_i, type_i, seating_i)
         # issue: it loads whole page and does not goes to specific part of the page
-        # cars(cost,brand, milege, type, seating)
+
+        # cars_found = test_data
+        cars_found = cars(
+            cost_min=cost_min_i,
+            cost_max=cost_max_i,
+            brand=brand_i,
+            mileage_min=mileage_min_i,
+            mileage_max=mileage_max_i,
+            p_d_cng=type_i,
+            seating=seating_i,
+        )
         
+        inp_data = {
+            "cost_min": "Price:" + str(cost_min_i),
+            "cost_max": "Max:" + str(cost_max_i),
+            "brand": brand_i,
+            "mileage_min": "Mileage:" + str(mileage_min_i),
+            "mileage_max": "Mileage:" + str(mileage_max_i),
+            "type": type_i,
+            "seating": seating_i,
+        }
+        print(cars_found)
+        # issue: the data is not loading inside the model 
+        return render_template("index.html", cars_found=cars_found, inp_data=inp_data)
+
+    # issue: its not taking space in the price_min
+    default_inp_data = {
+        "cost_min": "Price Min",
+        "cost_max": "Max",
+        "brand": "Brand",
+        "mileage_min": "Mileage_Min",
+        "mileage_max": "Max",
+        "type": "Petrol/CNG/Diesel",
+        "Seating_Cap": "Seating Capacity",
+    }
+    # issue: change defualt cars data
+    default_cars = test_data
+
+    return render_template(
+        "index.html", cars_found=default_cars, inp_data=default_inp_data
+    )
 
 
-    return render_template("index.html")
+# @app.route("/findcar/string:<post_slug>", methods= ["GET", "POST"])
+# def find_car(post_slug):
+#     inp_data= post_slug
+#     cars_found = cars(cost_max,cost_min, brand, mileage_min, mileage_max, type, seating)
+#     return render_template("findcar.html", cars_found=cars_found, inp_data= inp_data)
+
 
 @app.route("/blog")
 def blog():
     return render_template("blog.html")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
