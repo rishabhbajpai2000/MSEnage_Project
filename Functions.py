0f7e265f938e304a6c394bc042058d00edac0b1f
“Rishabh <rishabhbajpai190@gmail.com>
2022-05-28 18:28:39 +0530

    Find cars function works

--- Functions.py
+++ Functions.py
@@ -1,4 +1,3 @@
-# find car function
 # reading the file
 import pandas as pd
 main_data = pd.read_csv("Data.csv")
@@ -8,22 +7,55 @@ main_data = pd.read_csv("Data.csv")
 main_data["ARAI_Certified_Mileage_kmpl"].astype(float)
 # removing the duplicates based on the rows
 main_data = main_data.drop_duplicates(subset="Model")
+# for now top cars = data.head
+top_cars =main_data[main_data['Model'].isin(["Scorpio", "Thar", "Xuv500", "Fortuner","Vitara Brezza"])]
+# average top cars 
 
 
-test_data = main_data.head()
-#writing the main function
 def cars(cost_min, cost_max, brand, mileage_min, mileage_max, p_d_cng, seating, data=main_data):
-    filter_p = data[data["Ex-Showroom_Price"].isin(range(cost_min,cost_max))]
+    #validating the inputs
+    try:
+        cost_min = int(cost_min)
+    except:
+        return "You have entered invalid minimum cost, please re-enter"
+    try:
+        cost_max = int(cost_max)
+    except:
+        return "You have entered invalid maximum cost, please re-enter"
+    try:
+        mileage_min = int(mileage_min)
+    except:
+        return "You have entered invalid mimum mileage, please re-enter"
+    try:
+        mileage_max = int(mileage_max)
+    except:
+        return "you have entered invalid max mileage, please re-enter"
+
+    
+    # now giving results
+    filter_p = data[data["Ex-Showroom_Price"].isin(range(min(cost_min,cost_max), max(cost_min,cost_max)))]
+    if (len(filter_p) == 0):
+        return "none"
+
     filter_b_p = filter_p[filter_p["Make"] == brand]
+    if (len(filter_b_p) == 0):
+        return filter_p.sort_values("Ex-Showroom_Price").head()
+
     filter_b_p_t = filter_b_p[filter_b_p["Fuel_Type"]  == p_d_cng]
-    filter_b_m_p_t = filter_b_p_t[filter_b_p_t["ARAI_Certified_Mileage_kmpl"].isin(range(mileage_min, mileage_max))]
+    if (len(filter_b_p_t) == 0):
+        return filter_b_p.sort_values("Ex-Showroom_Price").head()
+
+    filter_b_m_p_t = filter_b_p_t[filter_b_p_t["ARAI_Certified_Mileage_kmpl"].isin(range(min(mileage_min, mileage_max), max(mileage_min, mileage_max)))]
+    if (len(filter_b_m_p_t) == 0):
+        return filter_b_p_t.sort_values("Ex-Showroom_Price").head()
+
     filter_b_m_p_s_t = filter_b_m_p_t[filter_b_m_p_t["Seating_Capacity"] == seating]
-    return filter_b_m_p_s_t.sort_values("Ex-Showroom_Price").head() 
+    if (len(filter_b_m_p_s_t) == 0):
+        return filter_b_m_p_t.sort_values("Ex-Showroom_Price").head()
 
 
 
-# simple comment
-# random commit from g cloud
+
 
 # def car_found_desc(data):
     # this fuction will take the output 5 rows data frame to generate a text description of it
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
-from Functions import cars, test_data
-import pandas as pd
-
+from Functions import cars, top_cars
 
 app = Flask(__name__)
 
@@ -10,18 +8,24 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         #  take the values from the form
-        cost_min_i = int(request.form.get("cost_min"))
-        cost_max_i = int(request.form.get("cost_max"))
+        cost_min_i = request.form.get("cost_min")
+        cost_max_i = request.form.get("cost_max")
         brand_i = request.form.get("brand")
-        mileage_min_i = int(request.form.get("mileage_min"))
-        mileage_max_i = int(request.form.get("mileage_max"))
+        mileage_min_i = request.form.get("mileage_min")
+        mileage_max_i = request.form.get("mileage_max")
         type_i = request.form.get("type")
-        seating_i = int(request.form.get("Seating_Cap"))
-        print(cost_max_i,cost_min_i, brand_i, mileage_min_i, mileage_max_i, type_i, seating_i)
-        # issue: it loads whole page and does not goes to specific part of the page
+        seating_i = request.form.get("Seating_Cap")
+        print(
+            cost_max_i,
+            cost_min_i,
+            brand_i,
+            mileage_min_i,
+            mileage_max_i,
+            type_i,
+            seating_i,
+        )
 
-        # cars_found = test_data
-        cars_found = cars(
+        cars_searched = cars(
             cost_min=cost_min_i,
             cost_max=cost_max_i,
             brand=brand_i,
@@ -40,25 +44,39 @@ def home():
             "type": type_i,
             "seating": seating_i,
         }
-        print(cars_found)
-        # issue: the data is not loading inside the model 
-        return render_template("index.html", cars_found=cars_found, inp_data=inp_data)
+        if (cars_searched == "none"):
+            cars_line = "We were not able to find a car that meets your expectations, but here are our best cars if you want."
+            return render_template("index.html",inp_data= inp_data, cars_line = cars_line, cars_found = top_cars, count = len(cars_searched))
+
+        if (type(cars_searched) == str):
+            return render_template("index.html", cars_line = cars_searched, inp_data= inp_data, count = 0 )
+        print(cars_searched)
+        
+        cars_line = "Based on your choices, our car recemmendations are"
+        return render_template(
+            "index.html", cars_found=cars_searched, inp_data=inp_data, cars_line=cars_line, count = len(cars_searched)
+        )
 
     # issue: its not taking space in the price_min
     default_inp_data = {
-        "cost_min": "Price Min",
+        "cost_min": "Min",
         "cost_max": "Max",
         "brand": "Brand",
-        "mileage_min": "Mileage_Min",
+        "mileage_min": "Min",
         "mileage_max": "Max",
         "type": "Petrol/CNG/Diesel",
         "Seating_Cap": "Seating Capacity",
     }
     # issue: change defualt cars data
-    default_cars = test_data
+    default_cars = top_cars
+    cars_line = "Our top 5 models from the collection"
 
     return render_template(
-        "index.html", cars_found=default_cars, inp_data=default_inp_data
+        "index.html",
+        cars_found=default_cars,
+        inp_data=default_inp_data,
+        cars_line=cars_line,
+        count=5
     )
 
 
